a4f5f8ea342da11e5b1b1e8c6d3c5863e8926d6d
Dan Hughes <danhughes@monzo.com>
2019-06-04 23:24:33 +0100

    Listen on webhook URL

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
-const API = require("./api");
-const assert = require("assert");
+const micro = require("micro");
+const { router, post } = require("microrouter");
+const cap = require("./cap");
 
-const {
-  ACCESS_TOKEN,
-  ACCOUNT_ID,
-  ACCOUNT_NUMBER,
-  CAP,
-  DESTINATION_POT_ID,
-  SORT_CODE
-} = process.env;
-assert(ACCESS_TOKEN);
-assert(ACCOUNT_ID);
-assert(ACCOUNT_NUMBER);
-assert(CAP);
-assert(DESTINATION_POT_ID);
-assert(SORT_CODE);
-
-const monzo = new API({
-  accessToken: ACCESS_TOKEN,
-  accountId: ACCOUNT_ID
-});
-
-async function main() {
-  const { balance } = await monzo.balance();
-  assert(balance);
-
-  // API takes amounts in pennies.
-  const cap = CAP * 100;
-
-  const excess = balance - cap;
-
-  if (excess <= 0) {
-    console.log(`Not sweeping: Excess is not positive (${excess})`);
-    return;
-  }
-
-  await monzo.deposit(DESTINATION_POT_ID, excess);
-  console.log(`Swept ${excess} into ${DESTINATION_POT_ID}`);
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-main();
+module.exports = router(
+  post("/webhook", async (req, res) => {
+    await cap();
+
+    return "OK";
+  })
+);
